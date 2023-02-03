a206627d08027802b5222bd7ec6590ced3531da1
Justus <galaxycheesemc@gmail.com>
2023-02-02 18:31:37 -0700

    Rewrite

--- server.js
+++ server.js
@@ -1,57 +1,60 @@
-const fs = require('fs');
-const util = require('util');
 const express = require('express');
-const path = require('path');
+const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
-const readFile = util.promisify(fs.readFile);
-const writeFile = util.promisify(fs.writeFile);
-const PORT = process.env.PORT || 3001;
+
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-//Read db and parse it using json
-const fetchNotes = () => {
-  return readFile('db/db.json', 'utf-8')
-  .then(rawNotes => [].concat(JSON.parse(rawNotes)))
-}
-
-//Middleware
-app.use(express.static('public'));
-app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 
-//GET Routes
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
-
-
-//Routing
 app.get('/api/notes', (req, res) => {
-    fetchNotes().then(notes => res.json(notes))
-    .catch(err => res.status(500).json(err))
-    });
-    
-app.post('/api/notes', ({body}, res) => {
-    fetchNotes()
-    .then(savedNotes => {
-    const newNotes = [...savedNotes, {title: body.title, text: body.text, id: uuidv4()}]
-    writeFile('db/db.json', JSON.stringify(newNotes)).then(() => res.json({msg: 'ok'}))
-    .catch(err => res.status(500).json(err))
-    })
-})
-//Bonus
-app.delete('/api/notes/:id', (req, res) => {
-    fetchNotes().then(savedNotes => {
-      let newNotes = savedNotes
-      .filter(note => note.id !== req.params.id)
-      writeFile('db/db.json', JSON.stringify(newNotes))
-      .then(() => res.json({msg: 'ok'}))
-      .catch(err => res.status(500).json(err))
-    })
-  })
+  fs.readFile('db/db.json', 'utf-8', (err, data) => {
+    if (err) {
+      res.status(500).json({ error: err });
+    } else {
+      res.json(JSON.parse(data));
+    }
+  });
+});
 
-app.listen(PORT, () => console.log(`App listening on port: ${PORT}.`));
+app.post('/api/notes', (req, res) => {
+  fs.readFile('db/db.json', 'utf-8', (err, data) => {
+    if (err) {
+      res.status(500).json({ error: err });
+    } else {
+      const notes = JSON.parse(data);
+      const newNote = {
+        title: req.body.title,
+        text: req.body.text,
+        id: uuidv4()
+      };
+      notes.push(newNote);
+      fs.writeFile('db/db.json', JSON.stringify(notes), (err) => {
+        if (err) {
+          res.status(500).json({ error: err });
+        } else {
+          res.json({ msg: 'ok' });
+        }
+      });
+    }
+  });
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  fs.readFile('db/db.json', 'utf-8', (err, data) => {
+    if (err) {
+      res.status(500).json({ error: err });
+    } else {
+      const notes = JSON.parse(data).filter(note => note.id !== req.params.id);
+      fs.writeFile('db/db.json', JSON.stringify(notes), (err) => {
+        if (err) {
+          res.status(500).json({ error: err });
+        } else {
+          res.json({ msg: 'ok' });
+        }
+      });
+    }
+  });
+});
+
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
