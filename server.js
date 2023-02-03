458260dda5a4b1e2c3b57554a864e8ccb533ebaa
Justus <galaxycheesemc@gmail.com>
2023-02-02 18:43:39 -0700

    Organizing code

--- server.js
+++ server.js
@@ -1,60 +1,63 @@
 const express = require('express');
-const fs = require('fs');
-const { v4: uuidv4 } = require('uuid');
-
-const app = express();
+const path = require('path');
+const files = require('fs');
+const utility = require('util');
+const readFile = utility.promisify(files.readFile);
+const writeFile = utility.promisify(files.writeFile);
+const { v4: uniqueId } = require('uuid');
+const myApp = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
+//Middleware
+myApp.use(express.json());
+myApp.use(express.static('public'));
+myApp.use(express.urlencoded({extended:false}));
 
-app.get('/api/notes', (req, res) => {
-  fs.readFile('db/db.json', 'utf-8', (err, data) => {
-    if (err) {
-      res.status(500).json({ error: err });
-    } else {
-      res.json(JSON.parse(data));
-    }
-  });
-});
+//Read datebase file and parse it using json
+const findNotes = () => {
+  return readFile('db/db.json', 'utf-8')
+  .then(baseNotes => [].concat(JSON.parse(baseNotes)))
+}
 
-app.post('/api/notes', (req, res) => {
-  fs.readFile('db/db.json', 'utf-8', (err, data) => {
-    if (err) {
-      res.status(500).json({ error: err });
-    } else {
-      const notes = JSON.parse(data);
-      const newNote = {
-        title: req.body.title,
-        text: req.body.text,
-        id: uuidv4()
-      };
-      notes.push(newNote);
-      fs.writeFile('db/db.json', JSON.stringify(notes), (err) => {
-        if (err) {
-          res.status(500).json({ error: err });
-        } else {
-          res.json({ msg: 'ok' });
-        }
-      });
-    }
-  });
-});
 
-app.delete('/api/notes/:id', (req, res) => {
-  fs.readFile('db/db.json', 'utf-8', (err, data) => {
-    if (err) {
-      res.status(500).json({ error: err });
-    } else {
-      const notes = JSON.parse(data).filter(note => note.id !== req.params.id);
-      fs.writeFile('db/db.json', JSON.stringify(notes), (err) => {
-        if (err) {
-          res.status(500).json({ error: err });
-        } else {
-          res.json({ msg: 'ok' });
-        }
-      });
-    }
-  });
-});
+//GET Route for home page
+myApp.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
-app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
+//Get Route for notes page
+myApp.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+
+//Display notes
+myApp.get('/api/notes', (req, res) => {
+    findNotes()
+    .then(notes => res.json(notes))
+    .catch(err => res.status(400).json(err))
+    });
+    
+//Updating notes request
+myApp.post('/api/notes', ({body}, res) => {
+    findNotes()
+    .then(currentNotes => {
+    const newNotes = [...currentNotes, {title: body.title, text: body.text, id: uniqueId()}]
+    writeFile('db/db.json', JSON.stringify(newNotes))
+    .then(() => res.json({msg: 'ok'}))
+    .catch(err => res.status(400).json(err))
+    })
+})
+
+//Bonus
+myApp.delete('/api/notes/:id', (req, res) => {
+    findNotes().then(currentNotes => {
+      let newNotes = currentNotes
+      .filter(note => note.id !== req.params.id)
+      writeFile('db/db.json', JSON.stringify(newNotes))
+      .then(() => res.json({msg: 'ok'}))
+      .catch(err => res.status(400).json(err))
+    })
+  })
+
+myApp.listen(PORT, () => console.log(`App port: ${PORT}!`));
